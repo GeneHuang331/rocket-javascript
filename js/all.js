2524e0cc48fd847dde154a4c447d2e83c39135ac
Gene <tutor8110221510@gmail.com>
2019-02-27 18:05:16 +0800

    v1

---
+++ js/all.js
@@ -0,0 +1,59 @@
+var bodyEl = document.body;
+bodyEl.addEventListener('keydown',shootRocket);
+function shootRocket(e) {
+    var key = e.keyCode;
+    console.log(key);
+    switch (key) {
+        case 49:
+            document.querySelector('.rocket-1').style.bottom = '1800px';
+            document.querySelector('.btn1').style.boxShadow = "3px 3px 5px #0a0a0a";
+            break;
+        case 97:
+            document.querySelector('.rocket-1').style.bottom = '1800px';
+            document.querySelector('.btn1').style.boxShadow = "3px 3px 5px #0a0a0a";
+            break;   
+        case 50:
+            document.querySelector('.rocket-2').style.bottom = '1800px';
+            document.querySelector('.btn2').style.boxShadow = "3px 3px 5px #0a0a0a"; 
+            break;
+        case 98:
+            document.querySelector('.rocket-2').style.bottom = '1800px';
+            document.querySelector('.btn2').style.boxShadow = "3px 3px 5px #0a0a0a";
+            break;       
+        case 51:
+            document.querySelector('.rocket-3').style.bottom = '1800px';
+            document.querySelector('.btn3').style.boxShadow ="3px 3px 5px #0a0a0a";
+            break;
+        case 99:
+            document.querySelector('.rocket-3').style.bottom = '1800px';
+            document.querySelector('.btn3').style.boxShadow = "3px 3px 5px #0a0a0a";
+            break;                         
+        default:
+            break;
+    }
+}
+
+var  btnEl = document.querySelectorAll('.btn');
+for (var i = 0; i<btnEl.length; i++){
+    btnEl[i].addEventListener('click', btnShootRocket);
+}
+
+function btnShootRocket(e){
+    var sel = e.target.value;
+    switch (sel){
+        case '1':
+            document.querySelector('.rocket-1').style.bottom = '1800px';
+            document.querySelector('.btn1').style.boxShadow = "3px 3px 5px #0a0a0a";
+            break;
+        case '2':
+            document.querySelector('.rocket-2').style.bottom = '1800px';
+            document.querySelector('.btn2').style.boxShadow = "3px 3px 5px #0a0a0a";
+            break;
+        case '3':
+            document.querySelector('.rocket-3').style.bottom = '1800px';
+            document.querySelector('.btn3').style.boxShadow = "3px 3px 5px #0a0a0a";
+            break;
+
+
+    }
+}
